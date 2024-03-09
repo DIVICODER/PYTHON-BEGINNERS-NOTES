bda991480f397ad2c747cd69572714836c27e327
DIVYA <131283175+DIVICODER@users.noreply.github.com>
2024-03-09 22:15:08 +0530

    Create COMMENTS.py

---
+++ COMMENTS.py
@@ -0,0 +1,27 @@
+#       COMMENTS IN PYTHON
+
+#---used to explain python code-- increase readablity--prevent execution when testing code.
+#---the lines in the comment wont occupy any space/memory--not read by interpreter.
+
+#CREATING A COMMENT
+
+    #1.we use " # " symbol
+print("above this line i have written command can you see that..")
+
+    #2. we can even place at the end of the code line
+    
+print("i have comment at the end of this..can you see it ")
+
+#MULTILINE COMMENTS:
+
+    #we can have " # " /" # en-number of times"--to write multiple line comment.
+
+    ######this is one the multiline command
+    ## as i have muliple command written one after the other.
+
+print("i have writeen a multiple line commend above this")
+
+"""this is an other way of writting multiple line of commends
+ but here we cannot give the space before this commend which we can give in '#' type of commend """
+
+print("i have multiple line of commend before this line can you see that commends in the interpreter")
